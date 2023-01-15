b53f3e784dd396234f0ed6cb900ae4ab1feeab3d
Jing Qiang <tohjingqiang@gmail.com>
2023-01-16 02:43:10 +0800

    Remove boilercode + initialise tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const options = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  bracketSpacing: true,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
+
+module.exports = options;
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div className="p-12 text-2xl text-red-500 font-bold">Test</div>;
+}
+
+export default App;
