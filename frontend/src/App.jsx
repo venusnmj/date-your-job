55eb53d72d080d65c4c0d77092f19b8196f53519
Jing Qiang <tohjingqiang@gmail.com>
2023-01-16 17:56:58 +0800

    fixed file extension from ts to js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import useFetch from './hooks/useFetch';
+import { useFetch } from './hooks';
 
 function App() {
   // const fetch = useFetch();
--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -1,7 +1,7 @@
 import { serverDomainUrl } from '../config/serverDomainUrl';
 
 // Helper functions
-const handleResponse = async (response: Response) => {
+const handleResponse = async (response) => {
   if (response.ok) return response.json();
 
   // Status not ok
@@ -16,31 +16,31 @@ const handleResponse = async (response: Response) => {
 
 // Custom fetch hook
 const useFetch = () => {
-  const get = async (url: string) => {
+  const get = async (url) => {
     const requestOptions = {
       method: 'GET',
-      credentials: 'include' as RequestCredentials,
+      credentials: 'include',
     };
     const response = await fetch(serverDomainUrl + url, requestOptions);
     return handleResponse(response);
   };
 
-  const post = async (url: string, body: unknown) => {
+  const post = async (url, body) => {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      credentials: 'include' as RequestCredentials,
+      credentials: 'include',
       body: JSON.stringify(body),
     };
     const response = await fetch(serverDomainUrl + url, requestOptions);
     return handleResponse(response);
   };
 
-  const put = async (url: string, body: unknown) => {
+  const put = async (url, body) => {
     const requestOptions = {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
-      credentials: 'include' as RequestCredentials,
+      credentials: 'include',
       body: JSON.stringify(body),
     };
     const response = await fetch(url, requestOptions);
@@ -48,10 +48,10 @@ const useFetch = () => {
   };
 
   // prefixed with underscored because delete is a reserved word in javascript
-  const _delete = async (url: string) => {
+  const _delete = async (url) => {
     const requestOptions = {
       method: 'DELETE',
-      credentials: 'include' as RequestCredentials,
+      credentials: 'include',
     };
     const response = await fetch(serverDomainUrl + url, requestOptions);
     return handleResponse(response);
