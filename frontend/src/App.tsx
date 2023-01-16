f0308b215727d4c47afa4f2744a87a7c473c638a
Jing Qiang <tohjingqiang@gmail.com>
2023-01-17 06:30:24 +0800

    added mobile navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,19 @@ import { SignInPage } from './pages';
 import { Route, Routes } from 'react-router-dom';
 import { SignUpPage } from './pages/SignUpPage';
 import { SettingsPage } from './pages/SettingsPage';
-import { Navbar } from './features';
+import { Navbar, NavbarMobile } from './features';
 
 function App() {
 
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-blue-500">
       <Navbar />
       <Routes>
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />}/>
         <Route path="/settings" element={<SettingsPage />}/>
       </Routes>
+      <NavbarMobile />
     </div>
   );
 }
