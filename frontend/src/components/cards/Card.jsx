2841a78c04be4e4af3b0fb9d81f34efe5abcfd86
Jing Qiang <47933193+xJQx@users.noreply.github.com>
2023-01-16 17:31:07 +0800

    Frontend add component template (#2)
    * added auth with jwt token cookies with refresh functionality + user modules
    
    * add Applicants module
    
    * add Employers module
    
    * frontend stuff
    
    * frontend add component template

---
+++ frontend/src/components/cards/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CardBody } from './CardBody';
+import { CardImage } from './CardImage';
+
+const Card = () => {
+  return (
+    <div id="card-container" className="">
+      <CardImage />
+      <CardBody />
+    </div>
+  )
+}
+
+export default Card;
